e8f5d2887cee20f1fe70d930b744f5573cd13b90
George Lam <georgelamd@gmail.com>
2024-08-31 16:32:34 -0400

    App will save states

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,37 @@ import { IndexContext } from "./contexts/IndexContext";
 export default function App() {
   const [rowIds, setRowIds] = useState([]);
   const [upvoteRows, setUpvoteRows] = useState([]);
+  const [rowStates, setRowStates] = useState([]);
 
   const LOCAL_STORAGE_KEY_IDS = "ids";
   const LOCAL_STORAGE_KEY_UPVOTES = "upvotes";
+  const LOCAL_STORAGE_KEY_STATES = "states";
 
   useEffect(() => {
     const initialRowIds = loadData(LOCAL_STORAGE_KEY_IDS);
     const initialUpvoteRows = loadData(LOCAL_STORAGE_KEY_UPVOTES);
+    const initialStates = loadData(LOCAL_STORAGE_KEY_STATES);
     const numRows = initialRowIds.length > 0 ? initialRowIds.length : 3; // The number of rows is variable
     if (initialRowIds.length === 0) {
       for (let i = 0; i < numRows; i++) {
         initialRowIds.push(i);
         initialUpvoteRows.push([]);
+        initialStates.push(false);
       }
       saveData(LOCAL_STORAGE_KEY_IDS, initialRowIds);
     }
     setRowIds(initialRowIds);
     setUpvoteRows(initialUpvoteRows);
+    setRowStates(initialStates);
   }, []);
 
   useEffect(() => {
     saveData(LOCAL_STORAGE_KEY_UPVOTES, upvoteRows);
   }, [upvoteRows]);
+
+  useEffect(() => {
+    saveData(LOCAL_STORAGE_KEY_STATES, rowStates);
+  }, [rowStates]);
 
   function loadData(key) {
     const storedData = localStorage.getItem(key);
@@ -50,13 +59,24 @@ export default function App() {
     });
   }
 
+  function updateState(index) {
+    setRowStates((prev) => {
+      return prev.map((state, i) => {
+        if (i === index) {
+          return !state;
+        }
+        return state;
+      });
+    })
+  }
+
   return (
-    <AppContext.Provider value={{ upvoteRows, addUpvote }}>
+    <AppContext.Provider value={{ upvoteRows, rowStates, addUpvote, updateState }}>
       <div className={styles.container}>
         {rowIds.map((index) => {
           // List should be static, so will use index as key for now
           return (
-            <IndexContext.Provider value={{ index }}>
+            <IndexContext.Provider key={index} value={{ index }}>
               <UpvoteRow key={index}></UpvoteRow>
             </IndexContext.Provider>
           );
--- src/components/UpvoteContainer.jsx
+++ src/components/UpvoteContainer.jsx
@@ -1,23 +1,17 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import UpvoteButton from "./UpvoteButton";
 import styles from "./UpvoteContainer.module.css";
 import { AppContext } from "../contexts/AppContext";
 import { IndexContext } from "../contexts/IndexContext";
 
 export default function UpvoteContainer() {
-  const [isSelected, setIsSelected] = useState(false);
-
   const { index } = useContext(IndexContext);
-  const { upvoteRows } = useContext(AppContext);
-
-  function handleOnClick() {
-    setIsSelected(!isSelected);
-  }
+  const { upvoteRows, rowStates, updateState } = useContext(AppContext);
 
   return (
     <div className={styles.container}>
       {upvoteRows[index].map((upvote) => {
-        return <UpvoteButton key={upvote.id} handleOnClick={handleOnClick} isSelected={isSelected}></UpvoteButton>;
+        return <UpvoteButton key={upvote.id} handleOnClick={() => updateState(index)} isSelected={rowStates[index]}></UpvoteButton>;
       })}
     </div>
   );
